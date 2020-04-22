40e640a157271fe06d112a6a4f72d255fde5e56d
Piotr Janik <piotrekj11@gmail.com>
2020-04-22 12:34:38 +0200

    add class

---
+++ src/js/script.js
@@ -0,0 +1,95 @@
+/* global Handlebars, utils, dataSource */ // eslint-disable-line no-unused-vars
+
+{
+  'use strict';
+
+  const select = {
+    templateOf: {
+      menuProduct: '#template-menu-product',
+    },
+    containerOf: {
+      menu: '#product-list',
+      cart: '#cart',
+    },
+    all: {
+      menuProducts: '#product-list > .product',
+      menuProductsActive: '#product-list > .product.active',
+      formInputs: 'input, select',
+    },
+    menuProduct: {
+      clickable: '.product__header',
+      form: '.product__order',
+      priceElem: '.product__total-price .price',
+      imageWrapper: '.product__images',
+      amountWidget: '.widget-amount',
+      cartButton: '[href="#add-to-cart"]',
+    },
+    widgets: {
+      amount: {
+        input: 'input[name="amount"]',
+        linkDecrease: 'a[href="#less"]',
+        linkIncrease: 'a[href="#more"]',
+      },
+    },
+  };
+
+  const classNames = {
+    menuProduct: {
+      wrapperActive: 'active',
+      imageVisible: 'active',
+    },
+  };
+
+  const settings = {
+    amountWidget: {
+      defaultValue: 1,
+      defaultMin: 1,
+      defaultMax: 9,
+    }
+  };
+
+  const templates = {
+    menuProduct: Handlebars.compile(document.querySelector(select.templateOf.menuProduct).innerHTML),
+  };
+
+  class Product{
+    constructor(id, data){
+      this.id = id;
+      this.data = data;
+      this.renderInMenu();
+      //console.log('new product:', this);
+    }
+    renderInMenu(){
+      const generatedHTML = templates.menuProduct(this.data);
+      //console.log(generatedHTML);
+      this.element = utils.createDOMFromHTML(generatedHTML);
+      const menuContainer = document.querySelector(select.containerOf.menu);
+      menuContainer.appendChild(this.element);
+      console.log(this);
+    }
+  }
+
+  const app = {
+    initMenu: function(){
+      //console.log('thisApp.data:', this.data);
+      for(let productData in this.data.products){
+        new Product(productData, this.data.products[productData]);
+      }
+    },
+    initData: function(){
+      this.data = dataSource;
+    },
+    init: function(){
+      const thisApp = this;
+      console.log('*** App starting ***');
+      console.log('thisApp:', thisApp);
+      console.log('classNames:', classNames);
+      console.log('settings:', settings);
+      console.log('templates:', templates);
+      thisApp.initData();
+      thisApp.initMenu();
+    },
+  };
+  
+  app.init();
+}
